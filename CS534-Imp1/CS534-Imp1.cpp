b0b1486a04958bbcf66cbeb90a542752e49eb267
Michael Mendes <mendesm@flip1.engr.oregonstate.edu>
2013-04-16 17:55:28 -0700

    Initial draft of batch/stochastic gradient descent algorithms.  Not certain to work properly yet.

--- CS534-Imp1/CS534-Imp1.cpp
+++ CS534-Imp1/CS534-Imp1.cpp
@@ -14,6 +14,9 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	// read in regression training
 	vector<vector<double>> trainingData;
+	vector<double> batchTrainingError;
+	double batchTestError;
+
 	ifstream trainingFile("regression-train.csv");
 	while(trainingFile.good()) {
 		double x1, x2, x3, y;
@@ -60,7 +63,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		testData.push_back(example);
 	}
 	random_shuffle(testData.begin(), testData.end());
-
+	
 
 	// read in classification training data
 	vector<pair<int, vector<double>>> twoGaussian;
@@ -126,20 +129,124 @@ int _tmain(int argc, _TCHAR* argv[])
 double batchGradientDescent(
 	vector<vector<double>> training, 
 	vector<vector<double>> test,
-	vector<double>& initWeights, 
-	vector<double>& finalWeights
+	vector<double> &weights,
+	double learningRate
 	) {
-	return 1.0f;
+		int i, j;
+		double result1, result2, sse;
+		vector<double> gradient (weights.size());
+		vector<double>::iterator wIterate;
+		
+		//  psuedocode:
+		// [outside: init weights to 0]
+		//	sse = 0
+		//	foreach (weights(size)) gradient = 0
+		//	run foreach(i) training: 
+		//			result1 = 0;
+		//		for (j) from 0 to weights(size-1) 
+		//			result1 += weights[j] * training[i][j]
+		//			result2 = result1 - training[i][weights(size-1)]
+		//		sse += 0.5 * pow(result2, 2)
+		//		for (j) from 0 to training[i] size-2
+		//			gradient[j] += result2 * training[i][j]
+		//	foreach(i) weights
+		//		weights[i] = weights[i] - (learnrate) * gradient[i]
+		//	[return from function, then store sse in vector.  End program by outputting error vector.]
+		sse = 0;
+		for(i = 0; i<weights.size(); i++){
+			gradient[i] = 0;
+		}
+		for(i = 0; i<training.size(); i++){
+			result1 = 0;
+			for(j = 0; j<weights.size(); j++){
+				result1 += weights[j] * training[i][j];
+				result2 = result1 - training[i][weights.size()-1];
+			}
+			sse += 0.5 * pow(result2, 2);
+			for(j = 0; j<training.size()-1; j++){
+				gradient[j] += result2 * training[i][j];
+			}
+		}
+		for(i = 0; i<weights.size(); i++){
+			weights[i] = weights[i] - learningRate * gradient[i];
+		}
+
+	return sse;
 }
 
 // takes initial weights, test & training data (already randomized), returns weight vector & total SSE error
 double stochasticGradientDescent(
 	vector<vector<double>> training, 
-	vector<vector<double>> test,
-	vector<double>& initWeights, 
-	vector<double>& finalWeights
+	vector<double> &weights,
+	double learningRate,
 	) {
-	return 1.0f;
+
+		int i, j;
+		double result1, result2, sse;
+		vector<double> gradient (weights.size());
+		vector<double>::iterator wIterate;
+		
+		//  psuedocode:
+		// [outside: init weights to 0]
+		//	sse = 0
+		//	foreach (weights(size)) gradient = 0
+		//	run foreach(i) training: 
+		//			result1 = 0;
+		//		for (j) from 0 to weights(size-1) 
+		//			result1 += weights[j] * training[i][j]
+		//			result2 = result1 - training[i][weights(size-1)]
+		//		sse += 0.5 * pow(result2, 2)
+		//		for (j) from 0 to training[i] size-2
+		//			gradient[j] += result2 * training[i][j]
+		//		foreach(j) weights
+		//			weights[j] = weights[j] - (learnrate) * gradient[j]
+		//		
+		//	[return from function, then store sse in vector.  End program by outputting error vector.]
+		sse = 0;
+		for(i = 0; i<weights.size(); i++){
+			gradient[i] = 0;
+		}
+		for(i = 0; i<training.size(); i++){
+			result1 = 0;
+			for(j = 0; j<weights.size(); j++){
+				result1 += weights[j] * training[i][j];
+				result2 = result1 - training[i][weights.size()-1];
+			}
+			sse += 0.5 * pow(result2, 2);
+			for(j = 0; j<training.size()-1; j++){
+				gradient[j] += result2 * training[i][j];
+			}
+			for(j = 0; j<weights.size(); j++){
+				weights[j] = weights[j] - learningRate * gradient[j];
+			}
+		}
+
+	// note: for simplicity, this is the sse per epoch.  
+	// more formally, an sse could be produced per update step
+	// we chose this method for consistency of comparison with the batch algorithm
+	return sse;
+}
+
+double testGradientDescent(
+		vector<vector<double>> test,
+		vector<double> &weights
+	){
+		int i, j;
+		double sse, result1, result2;
+
+		sse = 0;
+		for(i = 0; i<test.size(); i++){
+			result1 = 0;
+			for(j = 0; j<weights.size(); j++){
+
+			for(j = 0; j<weights.size(); j++){
+				result1 += weights[j] * test[i][j];
+				result2 = result1 - test[i][weights.size()-1];
+			}
+			sse += 0.5 * pow(result2, 2);
+		}
+
+		return sse;
 }
 
 // takes initial weights, training data, returns weight vector & total SSE error
